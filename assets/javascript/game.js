62b05c36b05eacbb95284f6dc74ea612dddd5fc5
Stuart Kushner <stuartkushner@Stuarts-MacBook-Pro.local>
2017-11-08 10:38:33 -0500

    revised the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,77 @@
-// Variables
 $(document).ready(function() {
 
-	var numberChosen;
-	var counter;
+//Variables
+	var counter = 0;
 	var wins = 0;
+	$("#wins").text(wins);
+
 	var losses = 0;
-	var crystalValues;
+	$("#losses").text(losses);
+
+	var numberChosen = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+	$("#number-chosen").text(numberChosen);
+
+	var redValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+	var blueValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+	var yellowValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+	var greenValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+
+//Functions
 
 function newGame() {
+	counter = 0;
+	$("#totalScore").text(counter);
+
 	numberChosen = Math.floor(Math.random() * (120 - 19 + 1) + 19);
 	$("#number-chosen").text(numberChosen);
 	console.log(numberChosen);
 
-	//Assign four random variables to each of the crystals
-	for (var i = 0; i < 4; i++) {
-		crystalValues = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-		console.log(crystalValues);
-
-		var crystalImages = $("<img>");
-
-		var imageSources = [
-		"assets/images/red-crystal.jpg", 
-		"assets/images/blue-crystal.jpg",
-		"assets/images/yellow-crystal.jpg",
-		"assets/images/green-crystal.jpg"
-		];
-
-		crystalImages.addClass("crystal-images");
-		crystalImages.attr("src", imageSources[i]);
-		crystalImages.attr("crystal-values", crystalValues[i]);
-		$(".crystals").append(crystalImages);
-	}
-
-	$(".crystal-images").on("click", function() {
-		var crystalValue = ($(this).attr("crystal-values"));
-		crystalValue = parseInt(crystalValue);
-		counter += crystalValue;
-		console.log(counter);
-		$("#totalScore").text(counter);
-
-		if (counter === numberChosen) {
-			alert("Congratulations! You Win!");
-			wins++;
-			$("#wins").text(wins);
-			newGame();
-		} else {
-			alert("I'm sorry. Please try again.");
-			losses++;
-			$("#losses").text(losses);
-			newGame();
-		}
-	});
+	redValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+	blueValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+	yellowValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+	greenValue = Math.floor(Math.random() * (12 - 1 + 1) + 1);
 }
 
-newGame();
+function winOrLose() {
+	if (counter === numberChosen) {
+		alert("Congratulations! You Win!");
+		wins++;
+		$("#wins").text(wins);
+		newGame();
+	} else if (counter > numberChosen) {
+		alert("I'm sorry. Please try again.");
+		losses++;
+		$("#losses").text(losses);
+		newGame();
+	}
+}
+	//Game
+	$(".red-crystal").on("click", function() {
+		counter += redValue;
+		console.log(counter);
+		$("#totalScore").text(counter);
+		winOrLose();
+	})
+
+	$(".blue-crystal").on("click", function() {
+		counter += blueValue;
+		console.log(counter);
+		$("#totalScore").text(counter);
+		winOrLose();
+	})
+
+	$(".yellow-crystal").on("click", function() {
+		counter += yellowValue;
+		console.log(counter);
+		$("#totalScore").text(counter);
+		winOrLose();
+	})
+
+	$(".green-crystal").on("click", function() {
+		counter += greenValue;
+		console.log(counter);
+		$("#totalScore").text(counter);
+		winOrLose();
+	});
 
 });
-	
